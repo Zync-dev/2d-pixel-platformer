e31b70e556318f27e9a527f3d3f1f1044ca02c3c
Zync <Lukasjuulnielsen@gmail.com>
2024-09-18 10:35:49 +0200

    Enemy and map gen

--- 2d-pixel-platformer/Assets/Scripts/EnemyChase.cs
+++ 2d-pixel-platformer/Assets/Scripts/EnemyChase.cs
@@ -5,26 +5,26 @@ using UnityEngine;
 public class EnemyChase : MonoBehaviour
 {
 
-    public float forceStrength;     // How fast we move
-    public Transform target;        // The thing you want to chase
+    public float speed;
+    public GameObject player;
 
-    private Rigidbody2D ourRigidbody;   // The rigidbody attached to this object
+    private Rigidbody2D rb;
 
 
     void Awake()
     {
-        // Get the rigidbody that we'll be using for movement
-        ourRigidbody = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
 
     void Update()
     {
-        // Move in the direction of our target
-
-        Vector2 direction = ((Vector2)target.position - (Vector2)transform.position).normalized;
-
-        // Move in the correct direction with the set force strength
-        ourRigidbody.AddForce(direction * forceStrength);
+        if(player.transform.position.x - this.transform.position.x <= 33)
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        } else
+        {
+            transform.Translate(Vector2.right * (speed+10) * Time.deltaTime);
+        }
     }
 }
--- 2d-pixel-platformer/Assets/Scripts/MapGeneration.cs
+++ 2d-pixel-platformer/Assets/Scripts/MapGeneration.cs
@@ -8,7 +8,7 @@ public class MapGeneration : MonoBehaviour
     public List<GameObject> segments;
     public float segmentLength = 18f;
     float currentSegmentSpawnLocation = 0;
-    public float spawnDistance = 20f;
+    public float spawnDistance = 100f;
     public GameObject playerModel;
 
     GameObject tileGrid;
@@ -22,9 +22,9 @@ public class MapGeneration : MonoBehaviour
     {
         if (playerModel != null && segments != null)
         {
-            if (playerModel.transform.position.x >= currentSegmentSpawnLocation - spawnDistance && playerModel.transform.position.x > currentSegmentSpawnLocation)
+            if (playerModel.transform.position.x >= currentSegmentSpawnLocation - spawnDistance)
             {
-                int rndSegment = Random.Range(0, segments.Count - 1);
+                int rndSegment = Random.Range(0, segments.Count);
 
                 GameObject spawnedSegment = Instantiate(segments[rndSegment], tileGrid.transform);
                 spawnedSegment.transform.position = new Vector3(currentSegmentSpawnLocation, 0f, 0f);
